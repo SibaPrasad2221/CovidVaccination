61e6f344a3efa3512bd107dfb0a8a866878fbe8c
sahoo <sahoosibaprasad2221@gmail.com>
2023-12-11 22:50:17 +0530

    Version 1

---
+++ src/main/java/com/vaccine/distributionsytem/CovidVaccinationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.vaccine.distributionsytem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovidVaccinationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CovidVaccinationSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vaccine/distributionsytem/CovidVaccinationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vaccine.distributionsytem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovidVaccinationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
